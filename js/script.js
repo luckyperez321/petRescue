53e90cc823b25287adcd1bb2aad6dfcb09c031d8
Lucky Perez <luckyperez321@gmail.com>
2024-04-11 09:11:30 -0500

    added isTired

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 const statusButton = document.querySelector("button");
 const pets = document.querySelector(".all-pets");
+
+const createPet = function ("name", "species") {
+    const pet = {
+        species: "species",
+        isTired: 5,
+
+        pet.sleep = function () {
+            return `<name> needs a nap. Zzz...`
+            this.isTired = 1;
+        }
+    };
+};
+//some mistakes here... want to completely debug when it's all said and done// this isn't commenting out
