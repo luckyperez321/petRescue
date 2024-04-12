ff3b25d9b7006eef737ecaf13c5d1fb39d619b43
Lucky Perez <luckyperez321@gmail.com>
2024-04-12 09:17:33 -0500

    not logging out what's expected

--- js/script.js
+++ js/script.js
@@ -1,37 +1,37 @@
 const statusButton = document.querySelector("button");
 const pets = document.querySelector(".all-pets");
 
-const createPet = function ("name", "species") {
+const createPet = function (name, species) {
     const pet = {
-        species: "species",
+        name: name,
+        species: species,
         isTired: 5,
 
-        pet.sleep = function () {
-            return `<name> needs a nap. Zzz...`
+        sleep: function () {
+            console.log ( `${this.name} needs a nap. Zzz...`);
             this.isTired = 1;
-        }
-        pet.play = function(){
-if (isTired === 10){
-    return `Too tired to play`;
-    this.sleep()
-} else ( isTired < 10){
-    console.log(Yay ${this.name} loves to play!);
-    pet()
-    
-}
-
-
-}
-
-        }
+        },
+        play: function () {
+            if (this.isTired === 10) {
+               console.log("Too tired to play");
+                this.sleep()
+            } else {
+            
+                console.log(`Yay ${this.name } loves to play!`);
+                this.isTired += 1;
+            }
+            }
+        };
+        return pet;
     };
 
 
+
 const sora = createPet("Sora", "ferret");
-const clover = ("Clover", "rabbit");
-const baxter = ("Baxter", "hamster");
-const cleo = ("Cleo", "rat");
-const francine = ("Francine", "turtle");
+const clover = createPet("Clover", "rabbit");
+const baxter = createPet("Baxter", "hamster");
+const cleo = createPet("Cleo", "rat");
+const francine = createPet("Francine", "turtle");
 
 console.log(sora, clover, baxter, cleo, francine);
-
+console.log (clover,baxter);
