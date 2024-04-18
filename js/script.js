adb541cc2e55835e0964114c7189d12228adfb78
Lucky Perez <luckyperez321@gmail.com>
2024-04-18 08:53:08 -0500

    need to work out undefined

--- js/script.js
+++ js/script.js
@@ -8,22 +8,22 @@ const createPet = function (name, species) {
         isTired: 5,
 
         sleep: function () {
-            console.log ( `${this.name} needs a nap. Zzz...`);
+            console.log(`${this.name} needs a nap. Zzz...`);
             this.isTired = 1;
         },
         play: function () {
             if (this.isTired === 10) {
-               console.log("Too tired to play");
+                console.log("Too tired to play");
                 this.sleep()
             } else {
-            
-                console.log(`Yay ${this.name } loves to play!`);
+
+                console.log(`Yay ${this.name} loves to play!`);
                 this.isTired += 1;
             }
-            }
-        };
-        return pet;
+        }
     };
+    return pet;
+};
 
 
 
@@ -32,6 +32,31 @@ const clover = createPet("Clover", "rabbit");
 const baxter = createPet("Baxter", "hamster");
 const cleo = createPet("Cleo", "rat");
 const francine = createPet("Francine", "turtle");
+//console.log(sora, clover, baxter, cleo, francine);
 
-console.log(sora, clover, baxter, cleo, francine);
-console.log (clover,baxter);
+clover.sleep();
+baxter.sleep();
+//console.log(clover,baxter);
+clover.isTired = 8;
+francine.isTired = 9;
+
+const allPets = ["sora", "clover", "baxter", "cleo", "francine"];
+//console.log(allPets);
+
+/*VERY CONFUSED ABOUT STEP 4 IN FIRST PART. DOES IT WANTT TO SAY OBJECT OR THE ACTUAL NAMES IN THE CONSOLE.LOG WHICH ONE IS CORRECT*/
+
+const showPets = function (petArray) {
+    pets.innerHTML = "";
+    for (let pet of petArray) {
+        let status = "ready to play!";
+        if (pet.isTired >= 7) {
+            status = "sleeping.";
+        }
+        const li = document.createElement ("li");
+        li.innerHTML = `<span class = "pet-name"> ${pet.name}</span> the ${pet.species} is ${status}`;
+        pet.append(li);
+    }
+};
+statusButton.addEventListener("click", function(){
+showPets(allPets);
+});
